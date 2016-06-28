961bf6e678492bc0b80036cd93ffbdab54b512d0
Charlie Baxter and bo zhao <charlie+bo@>
2016-06-28 09:28:27 -0700

    make list persistent and allow list to be deleted

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -1,4 +1,5 @@
 using Nancy;
+using System.Collections.Generic;
 using ToDoList.Objects;
 
 namespace ToDoList
@@ -9,8 +10,17 @@ namespace ToDoList
     {
       Get["/"] = _ => View["add_new_task.cshtml"];
       Get["/view_all_tasks"] = _ => {
-        Task newTask = new Task(Request.Query["new-task"]);
-        return View["view_all_tasks.cshtml", newTask];
+        List<string> allTasks = Task.GetAll();
+        return View["view_all_tasks.cshtml", allTasks];
+      };
+      Post["/task_added"] = _ => {
+        Task newTask = new Task (Request.Form["new-task"]);
+        newTask.Save();
+        return View["task_added.cshtml", newTask];
+      };
+      Post["/tasks_cleared"] = _ => {
+        Task.ClearAll();
+        return View["tasks_cleared.cshtml"];
       };
     }
   }
--- Objects/Task.cs
+++ Objects/Task.cs
@@ -1,8 +1,12 @@
+using System.Collections.Generic;
+
 namespace ToDoList.Objects
 {
   public class Task
   {
     private string _description;
+    private static List<string> _instances = new List<string> {};
+
     public Task (string description)
     {
       _description = description;
@@ -14,6 +18,18 @@ namespace ToDoList.Objects
     public void SetDescription(string newDescription)
     {
       _description = newDescription;
+    }
+    public static List<string> GetAll()
+    {
+      return _instances;
+    }
+    public void Save()
+    {
+      _instances.Add(_description);
+    }
+    public static void ClearAll()
+    {
+      _instances.Clear();
     }
   }
 }
